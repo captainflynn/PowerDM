e23f702dc94aecb7a734d43ee61d9fbc69621206
captainflynn <vincentnuszbaum@yer.nl>
2018-05-02 17:09:35 +0200

    added basic screens

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -1,3 +1,34 @@
-Import-Module $PSScriptRoot\Functions.psm1
+Import-Module .\Functions.psm1
 
-Get-PdfFieldNames -FilePath "$PSScriptRoot\Resources\sheet.pdf" -ITextLibraryPath "$PSScriptRoot\Resources\itextsharp.dll"
+#region XAML assemblies
+function Get-Homexaml{
+Add-Type -AssemblyName PresentationFramework
+[xml]$HomeXaml = Get-Content -Path "$PSScriptRoot\Views\PowerDM.xaml"
+$Homexamlreader = New-Object System.Xml.XmlNodeReader $HomeXaml
+$Global:Homewindow = [Windows.Markup.XamlReader]::Load($Homexamlreader)
+Write-Verbose $Homewindow
+}
+
+function Get-CharWizard{
+[xml]$CreateCharXaml = Get-Content -Path "$PSScriptRoot\Views\CreateChar.xaml"
+$CreateCharXamlReader = New-Object System.Xml.XmlNodeReader $CreateCharXaml
+$Global:CreateChar = [Windows.Markup.XamlReader]::Load($CreateCharXamlReader)
+Write-Verbose $CreateChar
+}
+
+#endregion
+
+Get-Homexaml
+function Add-HomeControlVariables {
+	New-Variable -Name 'btnNewChar' -Value $Homewindow.FindName('btnNewChar') -Scope 1 -Force
+	New-Variable -Name 'btnnLoadChar' -Value $Homewindow.FindName('btnLoadChar') -Scope 1 -Force
+}
+
+Add-HomeControlVariables
+
+$btnNewChar.add_Click({
+	Get-CharWizard
+	$CreateChar.ShowDialog()
+})
+
+$Homewindow.ShowDialog()
